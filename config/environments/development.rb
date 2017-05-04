369dfc90348736cff241d7dc1dd4fc2a73fcbe2c
Ganesh Navale <navale.developer@gamil.com>
2017-05-04 22:17:47 +0530

    configuration changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  Rails.application.config.action_cable.disable_request_forgery_protection = true
   # Show full error reports.
   config.consider_all_requests_local = true
 
