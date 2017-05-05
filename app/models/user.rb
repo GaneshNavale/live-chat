64a3828d218f3c0add4903aed0cc28abe379d827
Ganesh Navale <navale.developer@gmail.com>
2017-05-05 22:08:03 +0530

    corrected forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :rememberable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :trackable, :validatable
   has_many :messages
   has_many :chatrooms, through: :messages
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, :email, presence: true, uniqueness: true
 end
