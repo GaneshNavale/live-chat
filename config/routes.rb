501d52c56d58afd46b1387de2162350bf1416955
Ganesh Navale <navaleganesh2011@gmail.com>
2017-05-05 22:08:03 +0530

    corrected forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "welcome#about"
-  
-  get '/signup', to: "registrations#new"
-  post '/signup', to: "registrations#create"
-
-  get 'login', to: "sessions#new"
-  post '/login', to: "sessions#create" 
-  delete '/logout', to: "sessions#destroy"
 
   get 'users/:id', to: "users#show", as: "profile"
 
